a27030be5d7466fcf68dfe42806b76396d44c81b
Zack Abrams <36209780+zackabrams@users.noreply.github.com>
2025-06-02 14:12:46 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+ComfyUI KeySync Wrapper
+A wrapper for the KeySync lip-sync generation model in ComfyUI
+"""
+
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
